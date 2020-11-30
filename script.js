4408bd3e35f114e179333c2b0cf3229e6efcc447
jankefyn <72445405+jankefyn@users.noreply.github.com>
2020-11-30 11:37:03 +0100

    triangle interface

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 "use strict";
 console.log("Hallo Welt");
+let numberArray = [0, 10];
+let t1 = { obenX: 250, obenY: 50, linksX: 50, linksY: 150, rechtsX: 450, rechtsY: 150 };
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,1 +1,13 @@
 console.log("Hallo Welt");
+let numberArray: number[] = [0, 10];
+interface Triangle {
+obenX: number;
+obenY: number;
+linksX: number;
+linksY: number;
+rechtsX: number;
+rechtsY: number;
+}
+let t1: Triangle = {obenX: 250, obenY: 50, linksX: 50, linksY: 150, rechtsX: 450, rechtsY: 150};
+
+
