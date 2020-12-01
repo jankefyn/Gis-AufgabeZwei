0ad43a565489c72c303b4bd6768d99e91e3be709
jankefyn <72445405+jankefyn@users.noreply.github.com>
2020-12-01 10:55:34 +0100

    butttons in ts

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 "use strict";
-console.log("Hallo Welt");
-let numberArray = [0, 10];
+let buttonUnten = document.getElementById("buttonUnten");
+buttonUnten.addEventListener("click", öffneUnten);
+let buttonMitte = document.getElementById("buttonMitte");
+buttonMitte.addEventListener("click", öffneMitte);
+let buttonOben = document.getElementById("buttonOben");
+buttonOben.addEventListener("click", öffneOben);
+let buttonAll = document.getElementById("buttonAll");
+buttonAll.addEventListener("click", öffneAll);
+function öffneUnten() {
+    window.open("AuswahlOben.html");
+}
+function öffneMitte() {
+    window.open("AuswahlOben.html");
+}
+function öffneOben() {
+    window.open("AuswahlOben.html");
+}
+function öffneAll() {
+    window.open("AuswahlOben.html");
+}
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,4 +1,25 @@
-console.log("Hallo Welt");
+
+let buttonUnten: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonUnten");
+buttonUnten.addEventListener("click", öffneUnten);
+let buttonMitte: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonMitte");
+buttonMitte.addEventListener("click", öffneMitte);
+let buttonOben: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonOben");
+buttonOben.addEventListener("click", öffneOben);
+let buttonAll: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonAll");
+buttonAll.addEventListener("click", öffneAll);
+function öffneUnten(): void {
+    window.open("AuswahlOben.html");
+}
+function öffneMitte(): void {
+    window.open("AuswahlOben.html");
+}
+function öffneOben(): void {
+    window.open("AuswahlOben.html");
+}
+function öffneAll(): void {
+    window.open("AuswahlOben.html");
+}
+
 
 
 
