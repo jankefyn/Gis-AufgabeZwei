5124d9af7391a9dc65067ebabe87290a97136ec4
jankefyn <72445405+jankefyn@users.noreply.github.com>
2020-12-01 16:07:32 +0100

    Bilder arrays

--- data.ts
+++ data.ts
@@ -1,5 +1,23 @@
 namespace p2_3 {
     export let arrayBilderOben: Bild[] = [];
-    let bild1Oben: Bild = { link: "Bilder/bild1oben.jpg", typ: keyTypOben };
+    let bild1Oben: Bild = { link: "Bilder/bild1Oben.jpg", typ: keyTypOben };
     arrayBilderOben.push(bild1Oben);
+    let bild2Oben: Bild = { link: "Bilder/bild2Oben.jpg", typ: keyTypOben };
+    arrayBilderOben.push(bild2Oben);
+    let bild3Oben: Bild = { link: "Bilder/bild3Oben.jpg", typ: keyTypOben };
+    arrayBilderOben.push(bild3Oben);
+    export let arrayBilderUnten: Bild[] = [];
+    let bild1Unten: Bild = { link: "Bilder/Bild1Unten.jpg", typ: keyTypUnten};
+    arrayBilderUnten.push(bild1Unten);
+    let bild2Unten: Bild = { link: "Bilder/Bild2Unten.jpg", typ: keyTypUnten};
+    arrayBilderUnten.push(bild2Unten);
+    let bild3Unten: Bild = { link: "Bilder/Bild3Unten.jpg", typ: keyTypUnten};
+    arrayBilderUnten.push(bild3Unten);
+    export let arrayBilderMitte: Bild[] = [];
+    let bild1Mitte: Bild = { link: "Bilder/Bild1Mitte.jpg", typ: keyTypMitte};
+    arrayBilderUnten.push(bild1Mitte);
+    let bild2Mitte: Bild = { link: "Bilder/Bild2Mitte.jpg", typ: keyTypMitte};
+    arrayBilderUnten.push(bild2Mitte);
+    let bild3Mitte: Bild = { link: "Bilder/BildMitte.jpg", typ: keyTypMitte};
+    arrayBilderUnten.push(bild3Mitte);
 }
