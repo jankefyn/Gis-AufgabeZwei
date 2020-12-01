f4f784906ccb9699917ff0989026345f72686a00
jankefyn <72445405+jankefyn@users.noreply.github.com>
2020-12-01 09:44:48 +0100

    Grundgerüst der Website

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use strict";
 console.log("Hallo Welt");
 let numberArray = [0, 10];
-let t1 = { obenX: 250, obenY: 50, linksX: 50, linksY: 150, rechtsX: 450, rechtsY: 150 };
 //# sourceMappingURL=script.js.map
