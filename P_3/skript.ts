415c448f2ab6f49148a8861c0bc6b311d211bf1f
jankefyn <72445405+jankefyn@users.noreply.github.com>
2021-01-20 14:54:48 +0100

    versuch

--- P_3/skript.ts
+++ P_3/skript.ts
@@ -1,27 +1,16 @@
-import * as Http from "http";
+namespace P3_1 {
+    let formData: FormData = new FormData(document.forms[0]);
 
-export namespace P_3_1Server {
-    console.log("Starting server");
-    let port: number = Number(process.env.PORT);
-    if (!port)
-        port = 8100;
+    let btSend: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submit");
+    btSend.addEventListener("click", submit);
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
-
-    function handleListen(): void {
-        console.log("Listening");
-    }
-
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        _response.end();
-        console.log(Http.IncomingMessage);
+    async function submit(): Promise<void> {
+        let url: string = "https://gisaufgabedrei.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let text: string = await response.text();
+        console.log("Answer:");
+        console.log(text);
     }
 }
