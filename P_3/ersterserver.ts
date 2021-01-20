d4191c1e5c8443f8275060dd3893e3e8207b9726
jankefyn <72445405+jankefyn@users.noreply.github.com>
2021-01-20 14:59:57 +0100

    versuch2

--- P_3/ersterserver.ts
+++ P_3/ersterserver.ts
@@ -1,15 +1,16 @@
+  
 import * as Http from "http";
 
 export namespace P_3_1Server {
-    console.log("Starting server");
-    let port: number = Number(process.env.PORT);
+    console.log("Starting server"); // Konsolenausgabe
+    let port: number = Number(process.env.PORT); // Holt aktuellen Port
     if (!port)
-        port = 8100;
+        port = 8100; // Wenn kein Port, Port = 8100
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
+    let server: Http.Server = Http.createServer(); // Erstellt neuen HTTPServer
+    server.addListener("request", handleRequest); // Fuegt Listener hinzu
     server.addListener("listening", handleListen);
-    server.listen(port);
+    server.listen(port); // Horcht auf definierten Port
 
     function handleListen(): void {
         console.log("Listening");
@@ -17,11 +18,11 @@ export namespace P_3_1Server {
 
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
+        console.log("I hear voices!"); // Konsolenausgabe
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // Antwort als HTML
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        console.log(_request);
-        _response.end();
+        _response.write(_request.url); // Antwort URL ausgeben
+        console.log(_request.url); // URL auf Konsole ausgeben
+        _response.end(); // Antwort abschliessen
     }
 }
